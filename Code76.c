be2ae671d214290cf7f88eb8e5b7f2e99659b614
Abhinav Kumar <abhinavsinghsuryaabhi@gmail.com>
2026-06-19 18:54:08 +0530

    Find diagonal sum

---
+++ Code76.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    printf("Enter the order of the square matrix: ");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    printf("Enter the matrix elements:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        sum += matrix[i][i];
+    }
+
+    printf("\nSum of diagonal elements = %d\n", sum);
+
+    return 0;
+}
