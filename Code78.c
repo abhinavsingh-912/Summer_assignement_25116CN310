95b963ea2be57cf970767469e5c2cadea0f4d26f
Abhinav Kumar <abhinavsinghsuryaabhi@gmail.com>
2026-06-20 17:45:59 +0530

    Check symmetric matrix

---
+++ Code78.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, isSymmetric = 1;
+
+    printf("Enter the order of the square matrix: ");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (matrix[i][j] != matrix[j][i]) {
+                isSymmetric = 0;
+                break;
+            }
+        }
+        if (isSymmetric == 0)
+            break;
+    }
+
+    if (isSymmetric)
+        printf("The matrix is Symmetric.\n");
+    else
+        printf("The matrix is Not Symmetric.\n");
+
+    return 0;
+}
