87164c48154977bad0b1e08142f2622b664630ab
Abhinav Kumar <abhinavsinghsuryaabhi@gmail.com>
2026-06-23 16:58:03 +0530

    Remove duplicate characters

---
+++ Code96.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int visited[256] = {0};
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("String after removing duplicates: ");
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != '\n' && !visited[(unsigned char)str[i]]) {
+            printf("%c", str[i]);
+            visited[(unsigned char)str[i]] = 1;
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
